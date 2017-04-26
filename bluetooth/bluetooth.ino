142ce977f1143b9c12dc10bc003a2356d1fbfb94
Steven Arnold <slarnold@yahoo.com>
2017-04-26 21:59:07 +0200

    basic keyboard control, single key

--- bluetooth/bluetooth.ino
+++ bluetooth/bluetooth.ino
@@ -6,7 +6,6 @@
 // See www.martyncurrey.com for details
 //
  
- 
 #include <SoftwareSerial.h>
 SoftwareSerial BTserial(2, 3); // RX | TX
 // Connect the HC-05 TX to Arduino pin 2 RX. 
@@ -14,7 +13,28 @@ SoftwareSerial BTserial(2, 3); // RX | TX
 // 
  
 char c = ' ';
- 
+char prev = ' ';
+unsigned lasttime = 0;
+
+#define MOTORS 2
+int motor_speed[MOTORS] = {0,0};
+unsigned motor_offtime[MOTORS] = {0,0};
+
+void motor_on(int n, int speed, unsigned duration)
+{
+  motor_speed[n] = speed;
+  motor_offtime[n] = millis() + duration;
+  Serial.print("motor "); Serial.print(n); Serial.print(", offtime "); Serial.println(motor_offtime[n]);
+}
+
+void update_motor(int n)
+{
+  if (motor_speed[n] && (millis() >= motor_offtime[n])) {
+    motor_speed[n] = 0;
+    Serial.print("motor "); Serial.print(n); Serial.println(", off"); 
+  }
+}
+
 void setup() 
 {
     Serial.begin(9600);
@@ -24,15 +44,37 @@ void setup()
     // HC-05 default serial speed for AT mode is 38400
     BTserial.begin(9600);  
 }
- 
+
 void loop()
 {
- 
     // Keep reading from HC-05 and send to Arduino Serial Monitor
     if (BTserial.available())
     {  
         c = BTserial.read();
-        Serial.write(c);
+        switch (c) {
+          case 'u':
+          case 'd':
+          case 'l':
+          case 'r':
+            prev = c;
+            break;
+          case '1':
+            switch (prev) {
+              case 'u':
+                motor_on(0, 100, 100);
+                break;
+              case 'd':
+                motor_on(0, -100, 100);
+                break;
+              case 'l':
+                motor_on(1, 100, 100);
+                break;
+              case 'r':
+                motor_on(1, -100, 100);
+                break;              
+            }
+            break;
+        }
     }
  
     // Keep reading from Arduino Serial Monitor and send to HC-05
@@ -41,5 +83,8 @@ void loop()
         c =  Serial.read();
         BTserial.write(c);  
     }
- 
+
+    update_motor(0);
+    update_motor(1);
 }
+
--- bluetooth/btserial.js
+++ bluetooth/btserial.js
@@ -1,7 +1,10 @@
+var keypress = require('keypress');
 var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
 
 btSerial.on('found', function(address, name) {
+	console.log("found " + name + " @ " + address);
 	btSerial.findSerialPortChannel(address, function(channel) {
+		console.log('Serial port on channel '+channel);
 		btSerial.connect(address, channel, function() {
 			console.log('connected');
 
@@ -9,10 +12,6 @@ btSerial.on('found', function(address, name) {
 				if (err) console.log(err);
 			});
 
-			btSerial.on('data', function(buffer) {
-        console.log('Received: Size of data buf = ' + buffer.length);
-				console.log(buffer.toString('utf-8'));
-			});
 		}, function () {
 			console.log('cannot connect');
 		});
@@ -24,4 +23,59 @@ btSerial.on('found', function(address, name) {
 	});
 });
 
+btSerial.on('failure', function(err) {
+	console.log(err.toString());
+});
+
+btSerial.on('data', function(buffer) {
+	console.log('Received: Size of data buf = ' + buffer.length);
+	console.log(buffer.toString('utf-8'));
+});
+
+// make `process.stdin` begin emitting "keypress" events
+keypress(process.stdin);
+
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+//  console.log('got "keypress"', key);
+  if (key) {
+		switch (key.name) {
+			case 'q' :
+				console.log("closing btSerial");
+				btSerial.close();
+				process.stdin.setRawMode(false);
+				console.log("exit");
+				process.exit();
+				break;
+			case 'up' :
+				btSerial.write(new Buffer("u1", 'utf-8'), function(err, bytesWritten) {
+					console.log("u1");
+					if (err) console.log(err);
+				});
+				break;
+			case 'down' :
+				btSerial.write(new Buffer("d1", 'utf-8'), function(err, bytesWritten) {
+					console.log("d1");
+					if (err) console.log(err);
+				});
+				break;
+			case 'left' :
+				btSerial.write(new Buffer("l1", 'utf-8'), function(err, bytesWritten) {
+					console.log("l1");
+					if (err) console.log(err);
+				});
+				break;
+			case 'right' :
+				btSerial.write(new Buffer("r1", 'utf-8'), function(err, bytesWritten) {
+					console.log("r1");
+					if (err) console.log(err);
+				});
+				break;
+		}
+	}
+});
+
+console.log('inquire');
 btSerial.inquire();
+process.stdin.setRawMode(true);
+process.stdin.resume();
